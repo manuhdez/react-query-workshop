a9c9f102aa4c2e3202efa0bb92e2a1b368e69aba
Manuel Hernández <manuel@secret-source.eu>
2021-02-11 23:24:56 +0000

    core: install react-query and setup devtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
-import GlobalStyle from '../styles/GlobalStyle';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import GlobalStyle from 'styles/GlobalStyle';
 
 function MyApp({ Component, pageProps }) {
+  const client = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={client}>
       <GlobalStyle />
+      <ReactQueryDevtools initialIsOpen={false} />
       <Component {...pageProps} />
-    </>
+    </QueryClientProvider>
   );
 }
 
